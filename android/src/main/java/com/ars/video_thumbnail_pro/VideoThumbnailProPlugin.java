c57b5906822b89c1478a0cc4eee208c6e0c7afff
Abdul Rehman <arsarsars1@gmail.com>
2024-12-31 12:10:28 +0500

    Include VideoThumbnailProPlugin.java for plugin publish

--- android/src/main/java/com/ars/video_thumbnail_pro/VideoThumbnailProPlugin.java
+++ android/src/main/java/com/ars/video_thumbnail_pro/VideoThumbnailProPlugin.java
@@ -26,9 +26,9 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 /**
- * VideoThumbnailPlugin
+ * VideoThumbnailProPlugin
  */
-public class VideoThumbnailPlugin implements FlutterPlugin, MethodCallHandler {
+public class VideoThumbnailProPlugin implements FlutterPlugin, MethodCallHandler {
     private static final String TAG = "ThumbnailPlugin";
 
     private Context context;
